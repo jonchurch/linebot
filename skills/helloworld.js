e7b03223f4468229a627e248e0e3d4734befc28e
jonchurch <me@jonchurch.com>
2017-11-05 18:15:00 -0500

    test template carousel

--- skills/helloworld.js
+++ skills/helloworld.js
@@ -5,16 +5,72 @@ module.exports = (controller) => {
 		console.log({message})
 		bot.reply(message, 'Oh, Hello!')
 	})
-	
-	controller.hears('(.*)', 'message_received', (bot, message) => {
-		bot.reply(message, `Heard "${message.text}"`)
+	controller.hears('template', 'message_received', (bot, message) => {
+		bot.reply(message, {template:{
+  "altText": "this is a image carousel template",
+  "template": {
+      "type": "image_carousel",
+      "columns": [
+          {
+            "imageUrl": "https://placehold.it/500x500",
+            "action": {
+              "type": "postback",
+              "label": "Buy",
+              "data": "action=buy&itemid=111"
+            }
+          },
+          {
+            "imageUrl": "https://placehold.it/500x500",
+            "action": {
+              "type": "message",
+              "label": "Yes",
+              "text": "yes"
+            }
+          },
+          {
+            "imageUrl": "https://placehold.it/500x500",
+            "action": {
+              "type": "datetimepicker",
+              "label": "Date",
+              "data": "date",
+				"mode": "date"
+            }
+          },
+          {
+            "imageUrl": "https://placehold.it/500x500",
+            "action": {
+              "type": "datetimepicker",
+              "label": "Time",
+              "data": "time",
+				"mode": "time"
+            }
+          },
+          {
+            "imageUrl": "https://placehold.it/500x500",
+            "action": {
+              "type": "datetimepicker",
+              "label": "DateTime",
+              "data": "datetime",
+				"mode": "datetime"
+            }
+          },
+      ]
+  }
+} 
+		})
 	})
+
+	
 	controller.on('sticker', (bot, message) => {
 		bot.reply(message, 'Sticker received!')
 	})
 
 	controller.on('message_received', (bot, message)=> {
 		bot.reply(message, 'Received message')
+		console.log({message})
+	})
+	controller.hears('(.*)', 'message_received, line_postback', (bot, message) => {
+		bot.reply(message, `Heard "${message.text}"`)
 		console.log({message})
 	})
 }
