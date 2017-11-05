b2221eda51e698acde25e7ffa71624d5b64de478
jonchurch <me@jonchurch.com>
2017-11-05 15:34:29 -0500

    cleanup helloWorld

--- skills/helloworld.js
+++ skills/helloworld.js
@@ -1,14 +1,20 @@
 
 module.exports = (controller) => {
 
-	controller.on('message_received', (bot, message) => {
+	controller.hears('hi', 'message_received', (bot, message) => {
 		console.log({message})
-		bot.reply(message, 'Oh, and image')
+		bot.reply(message, 'Oh, Hello!')
 	})
 	
 	controller.hears('(.*)', 'message_received', (bot, message) => {
-		bot.reply(message, 'Heard hello!')
+		bot.reply(message, `Heard "${message.text}"`)
+	})
+	controller.on('sticker', (bot, message) => {
+		bot.reply(message, 'Sticker received!')
+	})
 
-
+	controller.on('message_received', (bot, message)=> {
+		bot.reply(message, 'Received message')
+		console.log({message})
 	})
 }
