4ddadc81a6859166db06b7cfd7f015196caf512c
jonchurch <me@jonchurch.com>
2017-11-07 00:22:10 -0500

    add button, carousel, and confirm template examples

--- skills/helloworld.js
+++ skills/helloworld.js
@@ -19,7 +19,118 @@ module.exports = (controller) => {
 			
 		})
 	})
-	controller.hears('template', 'message_received', (bot, message) => {
+
+	controller.hears('button', 'message_received', (bot, message) => {
+		bot.reply(message, {template: 
+	{
+  "altText": "this is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://placehold.it/500",
+      "title": "Menu",
+      "text": "Please select",
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
+}	
+		})
+	})
+
+controller.hears('confirm', 'message_received', (bot, message) => {
+	bot.reply(message, {template: 
+{
+  "altText": "this is a confirm template",
+  "template": {
+      "type": "confirm",
+      "text": "Are you sure?",
+      "actions": [
+          {
+            "type": "message",
+            "label": "Yes",
+            "text": "yes"
+          },
+          {
+            "type": "message",
+            "label": "No",
+            "text": "no"
+          }
+      ]
+  }
+}	
+	})
+})
+controller.hears('carousel', 'message_received', (bot, message) => {
+	bot.reply(message, {template: 
+{
+  "altText": "this is a carousel template",
+  "template": {
+      "type": "carousel",
+      "columns": [
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=111"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=111"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/111"
+                }
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=222"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=222"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                }
+            ]
+          }
+      ]
+  }
+}	
+	})
+})
+	controller.hears('image carousel', 'message_received', (bot, message) => {
 		bot.reply(message, {template:{
   "altText": "this is a image carousel template",
   "template": {
